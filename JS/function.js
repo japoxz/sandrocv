c6c1c82888277781158834bfa16a158573fc32b9
japoxz <126070316+japoxz@users.noreply.github.com>
2023-02-27 20:38:12 -0300

    Add files via upload

---
+++ JS/function.js
@@ -0,0 +1,7 @@
+document.getElementById ('telefono').onclick = function () {
+     console.log("muestra número de teléfono");
+     document.getElementById("tel").innerHTML = "0303456";}
+
+document.getElementById ('email').onclick = function () {
+          console.log("muestra dirección de e-mail");
+          document.getElementById("e-mail").innerHTML = "sandro@deamerica.com.ar";}
